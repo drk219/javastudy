b3a4cc0ad091fb6ba1bcceb9e92a5978c07c5206
drk219 <drk219@naver.com>
2024-02-01 19:03:49 +0900

    java

--- 11_Java_Standard_API/src/practice02_Lotto/Lotto.java
+++ 11_Java_Standard_API/src/practice02_Lotto/Lotto.java
@@ -1,5 +1,6 @@
 package practice02_Lotto;
 
+import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -19,17 +20,14 @@ public class Lotto {
      */
     
     Scanner sc = new Scanner(System.in);
-    System.out.println("로또 구매 비용을 입력하세요.");
-    
+    System.out.println("Lotto를 얼마나 구입하시겠습니까?(최대 10만원) >>> ");
     int money = sc.nextInt();
-    if(money >= 1000 && money <= 100000) {    // 구매 가능 금액은 최소 1000 ~ 최대 10000
-      money = (money / 1000) * 1000;          // 몫값 * 천원 => 1000 미만 단위는 버리고 반환
-      return money;
-    } else if (money < 1000){
-      throw new RuntimeException("구매 가능한 비용이 부족합니다.");
-    } else
-      throw new RuntimeException("구매 가능한 비용을 초과했습니다.");
-    
+    sc.close();
+    if(money < 1000 || money > 100000) {
+      System.out.println(money + "원 Lotto 구매는 불가능합니다.");
+      return 0;
+    }
+    return money / 1000 * 1000;
     
   }
   
@@ -67,26 +65,68 @@ public class Lotto {
      * 05 : 1  2  3  4  5  6
      */
     
-    Random random = new Random();
     List<String> papers = new ArrayList<String>();  // 로또 영수증 종이에 출력될 로또 배열
-    int lottoLine = money / 1000;                   // 출력할 로또 줄 수 = 구매금액 / 천원
     
-    for(int i = 0; i < lottoLine; i++) {
-      System.out.printf("%02d : ", i + 1);
-      Set<Integer> lottoNum = new HashSet<Integer>();
+    while(money > 0) {
       
-      while (lottoNum.size() < 6) {
-        int lottoNums =  random.nextInt(45) + 1;    // 번호는 1 ~ 45 중 한개
-        lottoNum.add(lottoNums);
-      }
+      // 한 게임 당 1000원
+      int game = (money >= 5000) ? 5 : money / 1000;
       
-      for(int number : lottoNum) {
-        System.out.print(number + " ");
+      // 게임 수에 맞는 로또 2차원 배열로 준비
+      int[][] lotto = new int[game][6];
+      
+      // 로또 2차원 배열 1~45 사이 난수 저장하는 for 문
+      for(int i = 0; i < game; i++) {
+        
+        SecureRandom secureRandom = new SecureRandom();
+        Set<Integer> set = new HashSet<Integer>();
+        while (set.size() != 6) {
+          set.add(secureRandom.nextInt(45) + 1);
+        }
+        
+        // 생성한 6개 난수를 lotto 2차원 배열에 저장한다.
+        int j = 0;
+        for(Integer number : set) {
+          lotto[i][j++] = number;
+        }
+     }
+     
+      // lotto 2차원 배열의 내용을 String 으로 만들어 StringBuilder 에 저장하는 for 문
+      StringBuilder sb = new StringBuilder();
+      for(int i = 0; i < game; i++) {
+        sb.append(String.format("%02d", i + 1) + " : ");
+          for(int j = 0; j < 6; j++) {
+            sb.append(String.format("%4d", lotto[i][j]));
+          }  
+          sb.append("\n");  // 5줄 마다 중간에 공백줄 삽입
       }
-      System.out.println();
+    
+      // StringBuilder 의 내용을 List papers 에 기록한다.
+      papers.add(sb.toString());
+      
+      money -= 5000;
     }
-    return papers;
     
+    return papers;
   }
-  
 }
+
+//    int lottoLine = money / 1000;                   // 출력할 로또 줄 수 = 구매금액 / 천원
+//    
+//    for(int i = 0; i < lottoLine; i++) {
+//      
+//      System.out.printf("%02d : ", i + 1);
+//      SecureRandom secureRandom =  new SecureRandom();
+//      Set<Integer> lottoNum = new HashSet<Integer>();
+//      
+//      while (lottoNum.size() < 6) {
+//        int lottoNums =  secureRandom.nextInt(45) + 1;    // 번호는 1 ~ 45 중 한개
+//        lottoNum.add(lottoNums);
+//      }
+//      
+//      for(int number : lottoNum) {
+//        System.out.print(number + " ");
+//      }
+//      System.out.println();
+//    }
+//    return papers;
